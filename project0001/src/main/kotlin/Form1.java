c1b89045d4737cfb06be792f1e3fb8168e9b205a
Marcelo Vicente Verri <marcelo_verri@hotmail.com>
2021-08-01 16:55:09 -0300

    Programa Criando Tela - resolver erros

--- project0001/src/main/kotlin/Form1.java
+++ project0001/src/main/kotlin/Form1.java
@@ -1,28 +1,29 @@
-import kotlin.Function;
 
+import kotlin.Function.*;
+import kotlin.jvm.functions.Function1.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Form1 {
-    private JTextField CampoA = new JTextField("",10);
-    private JTextField CampoB = new JTextField("",10);
-    private JButton Botao = new JButton("Somar");
+public class Form1 extends JFrame {
+    private JTextField campoA = new JTextField("",10);
+    private JTextField campoB = new JTextField("",10);
+    private JButton botao = new JButton("Somar");
 
-    public Function(){
+    public Form1(){
        super( "Mina Janela" );
        this.setSize(300,150);
        JPanel pn1 =new JPanel();
-       pn1.add(new JLabel("Valor 1: "))
-       pn1.add(CampoA);
+       pn1.add(new JLabel("Valor 1: "));
+       pn1.add(campoA);
        JPanel pn2= new JPanel();
-       pn2.add(new JLabel("Valor 1: "))
-       pn2.add(CampoB);
+       pn2.add(new JLabel("Valor 1: "));
+       pn2.add(campoB);
        JPanel pn3 = new JPanel();
-       pn3.add(Botao);
-       Botao.addActionListener(new ActionListener() {
+       pn3.add(botao);
+       botao.addActionListener(new ActionListener() {
            @Override
-           public void actionPerformed(ActionEvent e) {
+           public void actionPerformed(ActionEvent actionEvent) {
            executarAcao();
            }
        });
@@ -36,7 +37,7 @@ public class Form1 {
         JOptionPane.showMessageDialog(
                 this,
                 "somados o resultado"+
-                Function1Kt.executarAcao(
+                        Function1Kt.executarAcao(
                         campoA.getText(),
                         campoB.getText()
                 ),"Resultado da Soma",
